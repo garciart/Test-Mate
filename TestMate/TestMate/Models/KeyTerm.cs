d71ddd97b4732a6c7135fb208a130ce96b19fcf5
garciart <rgarcia@rgprogramming.com>
2019-09-22 22:29:19 -0400

    Fixed settings.xaml buttons and started adding classes to model.

---
+++ TestMate/TestMate/Models/KeyTerm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMate.Models {
+    /**
+     * TestMate model class for key term data objects
+     *
+     * @author Rob Garcia at rgarcia@rgprogramming.com
+     */
+    public class KeyTerm : TestData {
+
+    }
+}
---
+++ TestMate/TestMate/Models/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMate.Models {
+    /**
+     * TestMate model abstract class for test data objects
+     *
+     * @author Rob Garcia at rgarcia@rgprogramming.com
+     */
+    public abstract class TestData {
+
+    }
+}
